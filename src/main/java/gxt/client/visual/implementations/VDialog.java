88111f43ab85098b1ae904b3c00d1e6892962957
hanriseldon <hanriseldon@gmail.com>
2010-03-08 16:27:00 +0000

    - additional hide method added to enable testability of hide action

--- src/main/java/gxt/client/visual/implementations/VDialog.java
+++ src/main/java/gxt/client/visual/implementations/VDialog.java
@@ -1,5 +1,6 @@
 package gxt.client.visual.implementations;
 
+import gxt.client.visual.interfaces.IButton;
 import gxt.client.visual.interfaces.IDialog;
 
 import com.extjs.gxt.ui.client.widget.Dialog;
@@ -12,10 +13,14 @@ public class VDialog extends Dialog implements IDialog{
 	public VDialog(){
 		super();
 	}
-	
+
 	//
 	public Dialog getComponent(){
 		return this;
+	}
+	@Override
+	public void hide( final IButton buttonPressed ){
+		this.hide( buttonPressed.getComponent() );
 	}
 	
 }
