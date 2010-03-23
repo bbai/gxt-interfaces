2b53c6a4ae2552efbc331b62638a83dc82a4ab54
hanriseldon <hanriseldon@gmail.com>
2010-03-23 23:02:46 +0000

    - 1.0.5. (experimental) - additional work on the interfaces

---
+++ src/main/java/gxt/client/visual/interfaces/presenter/IContainerPresenter.java
@@ -0,0 +1,10 @@
+package gxt.client.visual.interfaces.presenter;
+
+import com.extjs.gxt.ui.client.widget.Component;
+
+/**
+ * @author eugenp
+ */
+public interface IContainerPresenter< T extends Component > extends IBoxComponentPresenter{
+	//
+}
