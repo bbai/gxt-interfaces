1df7485d5d39e9eab41a04458bfdb62536db1faf
hanriseldon@gmail.com <hanriseldon@gmail.com@f9c7e7a8-206b-11df-a7a9-25a45d9a8882>
2010-04-15 07:54:38 +0000

    - 1.13. - implementations for interfaces added

---
+++ trunk/src/main/java/gxt/client/visual/implementations/VMenuItem.java
@@ -0,0 +1,60 @@
+package gxt.client.visual.implementations;
+
+import gxt.client.visual.interfaces.IMenuItem;
+
+import com.extjs.gxt.ui.client.event.MenuEvent;
+import com.extjs.gxt.ui.client.event.SelectionListener;
+import com.extjs.gxt.ui.client.widget.menu.MenuItem;
+import com.google.gwt.user.client.ui.AbstractImagePrototype;
+
+/**
+ * @author eugenp
+ */
+public class VMenuItem extends MenuItem implements IMenuItem{
+	
+	public VMenuItem(){
+		this.canActivate = true;
+	}
+	
+	/**
+	 * Creates a new item with the given text.
+	 * @param text the item's text
+	 */
+	public VMenuItem( final String text ){
+		super( text );
+	}
+	
+	/**
+	 * Creates a new item.
+	 * @param text the item text
+	 * @param listener the selection listener
+	 */
+	public VMenuItem( final String text, final SelectionListener< ? extends MenuEvent > listener ){
+		super( text, listener );
+	}
+	
+	/**
+	 * Creates a new item.
+	 * @param text the item's text
+	 * @param icon the item's icon
+	 */
+	public VMenuItem( final String text, final AbstractImagePrototype icon ){
+		super( text, icon );
+	}
+	
+	/**
+	 * Creates a new item.
+	 * @param text the item's text
+	 * @param icon the item's icon
+	 * @param listener the selection listener
+	 */
+	public VMenuItem( final String text, final AbstractImagePrototype icon, final SelectionListener< ? extends MenuEvent > listener ){
+		super( text, icon, listener );
+	}
+	
+	//
+	public MenuItem getComponent(){
+		return this;
+	}
+	
+}
