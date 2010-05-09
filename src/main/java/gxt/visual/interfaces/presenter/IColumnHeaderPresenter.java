15c64ed89b861980f5831ad7c53a0f29e87d9ff0
hanriseldon <hanriseldon@gmail.com>
2010-05-09 16:10:07 +0000

    cleanup and large package cleanup and refactoring

--- src/main/java/gxt/visual/interfaces/presenter/IColumnHeaderPresenter.java
+++
@@ -1,8 +0,0 @@
-package gxt.visual.interfaces.presenter;
-
-/**
- * @author eugenp
- */
-public interface IColumnHeaderPresenter extends IBoxComponentPresenter{
-	//
-}
--- src/main/java/gxt/visual/interfaces/presenter/IVerticalPanelPresenter.java
+++
@@ -1,8 +0,0 @@
-package gxt.visual.interfaces.presenter;
-
-/**
- * @author eugenp
- */
-public interface IVerticalPanelPresenter extends ILayoutContainerPresenter{
-	//
-}
